ab9ba0e49d76dcd0cc6e2470c510adaddf9e9f4b
Misbachul Munir Al Haddad Alwi <megatenlike@gmail.com>
2024-06-14 13:23:07 +0700

    fix: param on get all

--- src/handler.js
+++ src/handler.js
@@ -1,6 +1,39 @@
 import { nanoid } from "nanoid";
 import books from "./books.js";
 
+const validateBook = (requestLoad) => {
+  let message;
+  if (typeof requestLoad.year !== "number" || requestLoad.year === 0) {
+    message = "Year wajib number dan tidak boleh 0";
+  }
+  if (typeof requestLoad.author !== "string" || requestLoad.author === "") {
+    message = "Author wajib string dan tidak boleh kosong";
+  }
+  if (typeof requestLoad.summary !== "string" || requestLoad.summary === "") {
+    message = "Summary wajib string dan tidak boleh kosong";
+  }
+  if (
+    typeof requestLoad.publisher !== "string" ||
+    requestLoad.publisher === ""
+  ) {
+    message = "Publisher wajib string dan tidak boleh kosong";
+  }
+  if (
+    typeof requestLoad.pageCount !== "number" ||
+    requestLoad.pageCount === 0
+  ) {
+    message = "pageCount wajib number dan tidak boleh 0";
+  }
+  if (typeof requestLoad.readPage !== "number" || requestLoad.readPage < 0) {
+    message = "readPage wajib number dan tidak boleh negatif";
+  }
+  if (typeof requestLoad.reading !== "boolean") {
+    message = "reading wajib boolean";
+  }
+
+  return message;
+};
+
 const addBookHandler = (request, h) => {
   const {
     name,
@@ -12,10 +45,23 @@ const addBookHandler = (request, h) => {
     readPage,
     reading,
   } = request.payload;
+
   const id = nanoid(16);
   const finished = pageCount === readPage;
   const insertedAt = new Date().toISOString();
   const updatedAt = insertedAt;
+
+  // VALIDATION
+  const errorMessage = validateBook(request.payload);
+
+  if (errorMessage !== undefined) {
+    const response = h.response({
+      status: "fail",
+      message: "Gagal menambahkan buku. " + errorMessage,
+    });
+    response.code(400);
+    return response;
+  }
 
   if (name === undefined) {
     const response = h.response({
@@ -75,16 +121,76 @@ const addBookHandler = (request, h) => {
   return response;
 };
 
-const getAllBooksHandler = () => ({
-  status: "success",
-  data: {
-    books: books.map((book) => ({
-      id: book.id,
-      name: book.name,
-      publisher: book.publisher,
-    })),
-  },
-});
+const getAllBooksHandler = (request, h) => {
+  const { name, reading, finished } = request.query;
+
+  if (name !== undefined) {
+    const filteredBooks = books.filter((book) =>
+      book.name.toLowerCase().includes(name.toLowerCase())
+    );
+
+    const response = h.response({
+      status: "success",
+      data: {
+        books: filteredBooks.map((book) => ({
+          id: book.id,
+          name: book.name,
+          publisher: book.publisher,
+        })),
+      },
+    });
+    response.code(200);
+    return response;
+  }
+
+  if (reading !== undefined) {
+    const response = h.response({
+      status: "success",
+      data: {
+        books: books
+          .filter((book) => book.reading === (reading === "1" ? true : false))
+          .map((book) => ({
+            id: book.id,
+            name: book.name,
+            publisher: book.publisher,
+          })),
+      },
+    });
+    response.code(200);
+    return response;
+  }
+
+  if (finished !== undefined) {
+    const response = h.response({
+      status: "success",
+      data: {
+        books: books
+          .filter((book) => book.finished === (finished === "1" ? true : false))
+          .map((book) => ({
+            id: book.id,
+            name: book.name,
+            publisher: book.publisher,
+          })),
+      },
+    });
+    response.code(200);
+    return response;
+  }
+
+
+  const response = h.response({
+    status: "success",
+    data: {
+      books: books.map((book) => ({
+        id: book.id,
+        name: book.name,
+        publisher: book.publisher,
+      })),
+    },
+  });
+  response.code(200);
+  return response;
+};
 
 const getBookByIdHandler = (request, h) => {
   const { id } = request.params;
@@ -111,8 +217,29 @@ const getBookByIdHandler = (request, h) => {
 const editBookByIdHandler = (request, h) => {
   const { id } = request.params;
 
-  const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
   const updatedAt = new Date().toISOString();
+
+  // VALIDATION
+  const errorMessage = validateBook(request.payload);
+
+  if (errorMessage !== undefined) {
+    const response = h.response({
+      status: "fail",
+      message: "Gagal menambahkan buku. " + errorMessage,
+    });
+    response.code(400);
+    return response;
+  }
 
   if (name === undefined) {
     const response = h.response({

dd370b525ba6d53d277ff67b5c9d2356a30736f7
Misbachul Munir Al Haddad Alwi <megatenlike@gmail.com>
2024-06-14 11:34:17 +0700

    first commit

---
+++ src/routes.js
@@ -0,0 +1,39 @@
+import handler from "./handler.js";
+
+const {
+  addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,
+} = handler;
+
+const routes = [
+  {
+    method: "POST",
+    path: "/books",
+    handler: addBookHandler,
+  },
+  {
+    method: "GET",
+    path: "/books",
+    handler: getAllBooksHandler,
+  },
+  {
+    method: "GET",
+    path: "/books/{id}",
+    handler: getBookByIdHandler,
+  },
+  {
+    method: "PUT",
+    path: "/books/{id}",
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookByIdHandler,
+  },
+];
+
+export default routes;
